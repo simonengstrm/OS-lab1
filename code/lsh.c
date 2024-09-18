56ce87392c8effe1970c880fdecbb0f26a2d5000
simonengstrm <simon_0_0_@live.se>
2024-09-18 09:10:47 +0200

    Added some preliminary features, not yet tested (backgrounding, built-ins, basic commands)

--- code/lsh.c
+++ code/lsh.c
@@ -23,6 +23,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
+#include <limits.h>
 
 // The <unistd.h> header is your gateway to the OS's process management facilities.
 #include <unistd.h>
@@ -33,12 +35,143 @@ static void print_cmd(Command *cmd);
 static void print_pgm(Pgm *p);
 void stripwhite(char *);
 
+// Current foreground pid
+static pid_t fg_pid = 0;
+
+void handle_child_finished(int sig)
+{
+  // Get status of child process using waitpid
+  int status;
+  // -1 for any child process
+  pid_t pid = waitpid(-1, &status, WNOHANG);
+  if (status == 0)
+  {
+    printf("\nChild process %d exited normally\n", pid);
+  }
+  else
+  {
+    printf("\nChild process %d exited with status %d\n", pid, status);
+  }
+  return;
+}
+
+void change_directory(Command *cmd)
+{
+  if (cmd->pgm->pgmlist[1] == NULL)
+  {
+    printf("No path provided\n");
+    const char *home = getenv("HOME");
+    if (home == NULL)
+    {
+      printf("Home variable not set\n");
+      return;
+    }
+    if (chdir(home) != 0)
+    {
+      printf("Chdir failed\n");
+      return;
+    }
+  }
+  else
+  {
+    if (chdir(cmd->pgm->pgmlist[1]) != 0)
+    {
+      printf("Chdir failed\n");
+      return;
+    }
+  }
+
+  return;
+}
+
+void handle_cmd(Command *cmd)
+{
+  // Check for built-in command
+  if (strcmp(cmd->pgm->pgmlist[0], "cd") == 0)
+  {
+    change_directory(cmd);
+    return;
+  }
+  else if (strcmp(cmd->pgm->pgmlist[0], "exit") == 0)
+  {
+    kill(0, SIGINT);
+    return;
+  }
+
+  // If not built in, commands should be handled by forking and doing the appropriate work
+  pid_t pid = fork();
+
+  if (pid == 0) // Child process
+  {
+    // Get and execute command, see if there are params
+    execvp(cmd->pgm->pgmlist[0], cmd->pgm->pgmlist);
+    // If exec returns, it has failed
+    printf("Could not execute program \"%s\"\n", cmd->pgm->pgmlist[0]);
+    _exit(-1);
+  }
+  else if (pid == -1) // Fork error
+  {
+    printf("Fork error\n");
+    kill(0, SIGINT);
+  }
+  else // Parent process
+  {
+    // Check if command is to be run in background
+    if (cmd->background == 1)
+    {
+      // Print pid of background process
+      printf("pid: %d\n", pid);
+      waitpid(pid, NULL, WNOHANG);
+    }
+    else
+    {
+      fg_pid = pid;
+      waitpid(pid, NULL, 0);
+    }
+  }
+}
+
+void handle_sigtstp(int sig)
+{
+  printf("Caught SIGTSTP\n");
+  exit(0);
+  return;
+}
+
+void handle_sigint(int sig)
+{
+  if (fg_pid != 0)
+  {
+    kill(fg_pid, SIGINT);
+  }
+  else
+  {
+    printf("\n");
+  }
+  return;
+}
+
 int main(void)
 {
+  // Setup signal handling
+  signal(SIGTSTP, handle_sigtstp);
+  signal(SIGINT, handle_sigint);
+  signal(SIGCHLD, handle_child_finished);
+
   for (;;)
   {
     char *line;
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+    printf("%s", cwd);
     line = readline("> ");
+
+    // Check EOF
+    if (line == NULL)
+    {
+      kill(0, SIGINT);
+      exit(0);
+    }
 
     // Remove leading and trailing whitespace from the line
     stripwhite(line);
@@ -53,6 +186,9 @@ int main(void)
       {
         // Just prints cmd
         print_cmd(&cmd);
+
+        // Handle commands
+        handle_cmd(&cmd);
       }
       else
       {
@@ -110,7 +246,6 @@ static void print_pgm(Pgm *p)
     printf("]\n");
   }
 }
-
 
 /* Strip whitespace from the start and end of a string.
  *
